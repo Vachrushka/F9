6b6d0807ca47f707aeca0ff5f65606dc06270b61
Alexey <polkovnikov6990@mail.ru>
2023-09-29 19:21:55 +0500

    trebuetsiy reshenie voprosa s zagruzkoi faila

--- app.py
+++ app.py
@@ -1,21 +1,27 @@
 from flask import Flask, render_template, request
 import os
+from flask_cors import CORS
+from flask_cors import cross_origin
+
 
 app = Flask (__name__)
+CORS(app)
 @app.route("/")
 def index():
     return render_template('index.html')
 
 @app.route('/upload',
 methods = ['POST'])
+@cross_origin(origin='*', methods=['POST'], headers=['Content-Type'])
 def upload():
     file=request.files['fileToUpload']
     if file:
-        filename=file.filename; file.save(os.path.join('/Users/Polkovnikovav/Desktop/Parser-F9-art-main/tmp', filename))
+        filename=file.filename; file.save(os.path.join('/tmp', filename))
         # Изменить путь при размещении в продакшн
         return 'Файл {} успешно загружен на сервер'.format(filename)
     else:
         return 'Файл не найден'
+
 
 
 if __name__ == "__main__":
