3b99fd59a46e0aacf65a8c2e2effa49b126b0cec
Alexey <polkovnikov6990@mail.ru>
2023-09-30 09:38:12 +0500

    ссс

--- app.py
+++ app.py
@@ -1,37 +1,22 @@
 from flask import Flask, render_template, request
 import os
-from flask_cors import CORS
-from flask_cors import cross_origin
-
 
 app = Flask (__name__)
-CORS(app)
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
+@app.route('/upload', 
+methods=['POST'])
 def upload():
-    file = request.files['file']
+    file = request.files['fileToUpload']
     if file:
         filename = file.filename
-        file.save("/tmp", filename)
+        file.save(os.path.join('/tmp', filename))
+       # file.save("/tmp", filename)
         return 'Файл успешно загружен на сервер'
     else:
         return 'Файл не найден'
-#@app.route('/upload',
-           
-#methods = ['POST'])
-#@cross_origin(origin='*', methods=['POST'], headers=['Content-Type'])
-#def upload():
-#    file=request.files['fileToUpload']
-#    if file:
-        filename=file.filename; file.save(os.path.join('/tmp', filename))
-        # Изменить путь при размещении в продакшн
-        return 'Файл {} успешно загружен на сервер'.format(filename)
- #   else:
-        return 'Файл не найден'
-
 
 
 if __name__ == "__main__":
